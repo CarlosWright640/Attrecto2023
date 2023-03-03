f1b3b27ed72912d1bcb53728a48d6554bcc98b54
Carlos Wright <71631323+CarlosWright640@users.noreply.github.com>
2023-03-03 18:44:03 +0100

    Attrecto ASP.NET 2. alkalom
    Saját CRUD felépítése.

---
+++ Attrecto-Academy-ASP-NET-2023/Academy-2023/Data/Couser.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy_2023.Data
+{
+    public class Course
+    {
+        [Required]
+        public int? Id { get; set; }
+
+        [StringLength(10)]
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        [StringLength(50)]
+        public string? Description { get; set; }
+    }
+}
---
+++ Attrecto-Academy-ASP-NET-2023/Academy-2023/Repositories/CourseRepository.cs
@@ -0,0 +1,68 @@
+﻿using Academy_2023.Data;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Academy_2023.Repositories
+{
+    public class CourseRepository
+    {
+        public static List<Course> Courses = new List<Course>();
+
+        public IEnumerable<Course> GetAll()
+        {
+            return Courses;
+        }
+
+        public Course? GetById(int id)
+        {
+            foreach (var course in Courses)
+            {
+                if (course.Id == id)
+                {
+                    return course;
+                }
+            }
+
+            return null;
+        }
+
+        public void Create(Course data)
+        {
+            Courses.Add(data);
+        }
+
+        public Course? Update(int id, Course data)
+        {
+            foreach (var course in Courses)
+            {
+                if (course.Id == id)
+                {
+                    course.FirstName = data.FirstName;
+                    course.LastName = data.LastName;
+                    course.Description = data.Description;
+
+                    return course;
+                }
+            }
+
+            return null;
+        }
+
+
+        public bool Delete(int id)
+        {
+            foreach (var course in Courses)
+            {
+                if (course.Id == id)
+                {
+                    Courses.Remove(course);
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
